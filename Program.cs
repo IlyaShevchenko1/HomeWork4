fd5c5c04dba338e579a6185c5593b2f588432755
Ilya Shevchenko <6BLK@mail.ru>
2023-08-06 21:20:09 +0300

    First task is completed

--- Program.cs
+++ Program.cs
@@ -3,6 +3,25 @@
 //Task 25. Напишите цикл, который принимает на вход два числа (A и B)
 //и возводит число A в натуральную степень B.
 
+int NumInPow(int firstNumber, int secondNumber)
+{
+    int result = firstNumber;
+    for (int i = 2; i <= secondNumber; i++)
+    {
+        result = result * firstNumber;
+    }
+    return result;
+}
+
+Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a power for the number: ");
+int power = Convert.ToInt32(Console.ReadLine());
+
+int result = NumInPow(number, power);
+Console.WriteLine($"Result of number {number} in power {power} is {result}");
+
+
 //Task 27. Напишите программу, которая принимает на вход число и выдает
 //сумму цифр в числе
 
